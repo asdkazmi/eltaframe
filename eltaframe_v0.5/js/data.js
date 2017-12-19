5ebc5fe1ba2a63e47c292a21e62a351716c63447
Syed Muhammad Asad <asdkazmi@gmail.com>
2017-12-19 20:51:56 +0500

    Add files via upload

---
+++ eltaframe_v0.5/js/data.js
@@ -0,0 +1,26 @@
+var create_class = {
+	color: {
+		main: "#795548",
+		main_b: "#313131"
+	},
+	width: {
+		icon: "100px"
+	},
+	height: {
+		nav: "120px" 
+	},
+	top : {
+		icon: "65px"
+	},
+	transform: {
+		icon_size: "scale(0.3)"
+	}
+}
+var main_data = {
+	navbar_url : {
+		Home: "index.html",
+		Download: "download.html",
+		Documentation: "documentation.html",
+		About: 'about.html'
+	}
+}
